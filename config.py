a3c6a286afe2d9136aae4cf0d3edea294a56a758
shadyxu <shadyxu@gmail.com>
2014-04-12 15:50:26 +0800

    完善config

--- config.py
+++ config.py
@@ -1,42 +1,35 @@
 # coding: utf-8
 
 
+# 支付宝分配用于MD5加密的秘钥
 SECURITY_KEY = ''
+
+# 手机支付交易地址
 TRADE_URL = 'http://222.66.233.198:8080/gateway/merchant/trade'
+
+# 手机支付查询地址
 QUERY_URL = 'http://222.66.233.198:8080/gateway/merchant/query'
+
+# 支付宝分配的商户ID
 MER_ID = ''
+
+# 前端/同步通知地址
 FRONT_END_URL = ''
+
+# 后端/异步通知地址
 BACK_END_URL = ''
-PREAUTH_BACK_END_URL = ''
 
-
+# 加密类型，支持"MD5", "0001"即RSA
 SIGN_TYPE = 'MD5'
 
+# 自定义的交易类型
 DEAL_TYPE = {
             'trade':'01',                    # 普通交易
             'preauth_trade':'02',            # 预授权发起 
             'preauth_complete':'03',         # 预授权完成
             'refund':'04',                   # 退货
-            'preauth_refund':'04',           # 预授权退货
+            'preauth_refund':'04',           # 预授权退货，同退货
             'cancel':'31',                   # 交易撤销
             'preauth_cancel':'32',           # 预授权撤销
             'preauth_complete_cancel':'33',  # 预授权完成撤销
 }
-
-
-DEAL_STATUS = {
-                'unpaid':0,
-                'paid':1,
-                'finished':2,
-                'fail':3,
-}
-
-
-RESP_SUCCESS = '00'
-
-TRANS_STATUS = {
-                'success':'00',
-                'processing':'01',
-                'fail':'03'
-                }
-
